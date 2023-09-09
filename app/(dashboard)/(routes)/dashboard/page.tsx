11de45c3cae05ef03e280fcf4c69eb8f33d8df06
Rehan <narutop224@gmail.com>
2023-09-09 01:21:12 -0400

    setup route groups and clerk auth

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+
+export default function DashboardPage() {
+  return (
+    <div>
+        <p>Dashboard page (Protected)</p>
+        <UserButton afterSignOutUrl='/' />
+    </div>
+    
+  )
+}
